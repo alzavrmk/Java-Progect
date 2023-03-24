0ce9d1c1b9b8a72d624d216f8d119270040cedb5
Anastasia <3845303@gmail.com>
2023-03-25 08:14:33 +0900

    add changes homework05

--- homework05/countName.java
+++ homework05/countName.java
@@ -1,19 +1,26 @@
 package homework05;
 import java.util.Map;
+
+//import homeWork03.ArrList;
+
 import java.util.HashMap;
 import java.io.BufferedReader;
 import java.io.FileReader;
-
-
 public class countName {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new FileReader("file1.txt"));
         String str;
-        Map <String, String> famalyName = new HashMap <> ();
+        Map <String, Integer> famalyName = new HashMap <> ();
         while ((str = br.readLine()) != null) {
             System.out.printf("== %s ==\n", str);
-            famalyName.put(str.split(" ")[0], str.split(" ")[1] );
+            String strkey =str.split(" ")[0];
+            if (famalyName.containsKey(strkey)) {
+                famalyName.put(strkey, famalyName.get(strkey)+1 );
+            } else {
+                famalyName.put(strkey, 1 );
+            }
         }
+        System.out.println(famalyName);
         br.close();
     }
 }
--- homework05/phonBook.java
+++ homework05/phonBook.java
@@ -1,8 +1,52 @@
 /* */
 package homework05;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class phonBook {
-    public static void main(String[ ] args) {
-        
+    public static void main(String[] args) {
+        Map<String, String> people = new HashMap<>();
+        Scanner sc = new Scanner(System.in);
+        Scanner scstr = new Scanner(System.in);
+        int numMenu = 0;
+        String strfamily = null;
+        String numberPhone = null;
+        while (true) {
+            try {
+                System.out.println("1- добавление номера,\n2 - вывод всего, \n3 - закончить ввод");
+                numMenu = sc.nextInt();          
+
+            } catch (Exception e) {
+                System.out.println("Ошибка ввода");// TODO: handle exception
+            }
+            if (numMenu == 1){
+                try {
+                    System.out.println("Фамилия:");
+                strfamily = scstr.nextLine();
+                System.out.println("Номер:");
+                numberPhone = scstr.nextLine(); 
+                } catch (Exception e) {
+                    System.out.println("Ошибка ввода");// TODO: handle exception
+                }
+                System.out.println("Фамилия:");
+                strfamily = scstr.nextLine();
+                System.out.println("Номер:");
+                numberPhone = scstr.nextLine();
+                people.put(strfamily, people.get(strfamily)+", "+numberPhone);
+            } else 
+                if(numMenu == 2){
+                System.out.println(people);
+                }
+                else { if (numMenu == 3) {
+                    break;
+                } else{
+                    System.out.println("Неверный ввод");
+                }
+                    
+                }
+
+        }
     }
 }
