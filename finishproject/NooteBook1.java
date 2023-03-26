c6d5e02534d1b868bfc5d73909789e07d1c6c805
Anastasia <3845303@gmail.com>
2023-03-26 23:19:56 +0900

    add modif

--- finishproject/NooteBook1.java
+++ finishproject/NooteBook1.java
@@ -6,14 +6,22 @@ public class NooteBook1 {
     public int ramValue;
     public int hddValue;
     public String color;
+    public int price;
     public String historyBreak;
 
-    public NooteBook1 (String title, String os, String color){
+    public NooteBook1 (String title, String os, String color, int ramValue, int hddValue, int price){
         this.title = title;
         this.os = os;
         this.color = color;
+        this.ramValue = ramValue;
+        this.hddValue = hddValue;
+        this.price = price;
+    
     }
 
+    public NooteBook1(){
+        this("None","None", "None", 0,0,0);
+    }
 
     public int getramValue(){
         return ramValue;
@@ -46,11 +54,23 @@ public class NooteBook1 {
     }
 
     private String getInfo(){
-       return String.format("Ноутбук: %s; \nОперационная система: %s;\nОЗУ: %s;\nОбъем жесткого диска: %s;\nЦвет: %s\n", title,os, ramValue,hddValue,color);
+       return String.format("Ноутбук: %s; \nОперационная система: %s;\nОЗУ: %s;\nОбъем жесткого диска: %s;\nЦвет: %s\n \n", title,os, ramValue,hddValue,color);
     }
 
     @Override
     public String toString(){
         return getInfo();
+    }
+
+    public NooteBook1 add (String strTitle, String strOs, String strcolor, int ramValue1, int hddValue1, int price){
+        NooteBook1 nb = new NooteBook1(strTitle,strOs,strcolor,ramValue1,hddValue1,price);
+        System.out.println(nb);
+        return nb;
+ /*       this.title = strTitle;
+        this.os = strOs;
+        this.color = strcolor;
+        this.ramValue = ramValue1;
+        */ 
+
     }
 }
--- finishproject/program.java
+++ finishproject/program.java
@@ -1,38 +1,68 @@
 package finishproject;
 
+import java.io.FileWriter;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
+import java.io.IOException;
 
 public class program {
     public static void main(String[] args) {
-        
-    
+        NooteBook1 nb = new  NooteBook1();
+        List<NooteBook1> nbList= new ArrayList<>();
 
-        Map<String, String> notebook = new HashMap<>();
+        
+        nbList.add(nb.add("Hp","Win10","black", 8, 512,18000));
+        nbList.add(nb.add("Asus","Linux","Gray", 16, 250, 60000));
+        nbList.add(nb.add("Honor","None OC","black", 8, 120, 40000));
+        nbList.add(nb.add("Huawei","Win11","black", 16, 250, 80000));
+        System.out.println(nbList);
+
         Scanner sc = new Scanner(System.in);
+/* 
+        
+        
         Scanner scTitle = new Scanner(System.in);
         Scanner scOs = new Scanner(System.in);
         Scanner scColor = new Scanner(System.in);
 
 
         int num = 0; 
-        int numMenu = 0;
+        
         String strtitle = null;
         String strOs = null;
         String strColor = null;
         String os = null;
+*/
+        int numMenu = 0;
+        Set<Integer> ramval = new HashSet<Integer>();
 
         while (true) {
             try {
-                System.out.println("1- добавить ноутбук,\n2 - вывод всего, \n3 - закончить ввод");
+                System.out.println("1- ОЗУ\n2 - Объем ЖД\n3 - Операционная система\n 4 - Цвет\n 5- выход");
                 numMenu = sc.nextInt();          
 
             } catch (Exception e) {
                 System.out.println("Ошибка ввода");
             }
             if (numMenu == 1){
-                try {
+                for (NooteBook1 nb1: nbList) {
+                    ramval.add(nb1.getramValue()); 
+                    //System.out.println(ramval);   
+
+                }
+            System.out.println(ramval);
+            }
+            else { if (numMenu == 5) {
+                    break;}}
+            
+   /*             try (FileWriter fw = new FileWriter("file.txt", true)){
+                
+                fw.append("nn");
                 System.out.println("Название ноутбука: ");
                 strtitle = scTitle.nextLine();
                 System.out.println("Операционная система: ");
@@ -45,29 +75,31 @@ public class program {
                 System.out.println("Объем жесткого диска: ");
                 n1.sethddValue(sc.nextInt()); 
                 notebook.put(strtitle, strOs+", "+strColor+", "+ n1.ramValue + n1.hddValue);
+                fw.append(strtitle+", "+strOs+", "+strColor+", "+ n1.ramValue + n1.hddValue);
             
                 } catch (Exception e) {
                     System.out.println("Ошибка ввода");
                 }
-                
-                
+
             } else 
                 if(numMenu == 2){
                 System.out.println(notebook);
                 }
-                else { if (numMenu == 3) {
-                    break;
+                
                 } else{
                     System.out.println("Неверный ввод");
                 }
                     
                 }
-        
-
         }
         scTitle.close();
         scOs.close();
         scColor.close();
-        sc.close();
+        
+
+*/ 
     }
+    sc.close();
 }
+}
+
